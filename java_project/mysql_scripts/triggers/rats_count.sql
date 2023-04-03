cfcf15f6a050bc5c5773d074bef691ea9fe4880f
guimmn2 <gmnss@protonmail.com>
2023-04-03 19:28:17 +0100

    rats_count trigger e .ini no gitignore

---
+++ java_project/mysql_scripts/triggers/rats_count.sql
@@ -0,0 +1,30 @@
+CREATE TRIGGER ratsCount AFTER INSERT ON medicoespassagens
+ FOR EACH ROW BEGIN
+
+DECLARE expID, salaEntradaExiste, salaSaidaExiste INT;
+SET expID = GetOngoingExpId();
+
+SELECT COUNT(1) into salaEntradaExiste
+FROM medicoessala
+WHERE medicoessala.idexperiencia = expID and medicoessala.sala = NEW.salaentrada;
+
+SELECT COUNT(1) into salaSaidaExiste
+FROM medicoessala
+WHERE medicoessala.idexperiencia = expID and medicoessala.sala = NEW.salasaida;
+
+IF salaEntradaExiste = 1 THEN
+    UPDATE medicoessala
+    SET medicoessala.numeroratosfinal = getRatsInRoom(NEW.salaentrada) + 1
+    WHERE medicoessala.idexperiencia = expID and medicoessala.sala = NEW.salaentrada;
+
+ELSEIF salaSaidaExiste = 1 THEN
+    UPDATE medicoessala
+    SET medicoessala.numeroratosfinal = getRatsInRoom(NEW.salasaida) - 1
+    WHERE medicoessala.idexperiencia = expID and medicoessala.sala = NEW.salasaida;
+
+ELSEIF salaEntradaExiste = 0 THEN
+    INSERT INTO medicoessala (idexperiencia, numeroratosfinal, sala)
+    VALUES (expID, 1, NEW.salaentrada);
+
+END IF;
+END
