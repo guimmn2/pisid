84675ebc38f3bdaa012cfd3d9f596bc5c926c7f7
guimmn2 <gmnss@protonmail.com>
2023-04-03 16:20:41 +0100

    scripts write (mov e temp) modificados para se poder fazer import

--- java_project/mysql_scripts/write_mov.sql
+++ java_project/mysql_scripts/write_mov.sql
@@ -1,6 +1,10 @@
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `WriteMov`(IN `hora` TIMESTAMP, IN `salaentrada` INT, IN `salasaida` INT)
+    NO SQL
 BEGIN
 IF OngoingExp() THEN
 	INSERT INTO medicoespassagens (datahora, salaentrada, salasaida)
-    VALUES (dataHora, salaEntrada, salaSaida);
+    VALUES (dataHora, salaentrada, salasaida);
 END IF;
-END
+END$$
+DELIMITER ;
