834a72a272666c519508066a556410af27401ff5
Guilherme Santos <72032143+gdsantos99@users.noreply.github.com>
2023-04-04 12:57:11 +0100

    added func get_rats:in_room

--- java_project/mysql_scripts/sps_and_funcs/get_rats_in_room.sql
+++ java_project/mysql_scripts/sps_and_funcs/get_rats_in_room.sql
@@ -1,11 +1,11 @@
 DELIMITER $$
-CREATE DEFINER=`root`@`localhost` FUNCTION `getRatsInRoom`(`nrSala` INT) RETURNS int
+CREATE DEFINER=`root`@`localhost` FUNCTION `getRatsInRoom`(`nrSala` INT) RETURNS int(11)
 BEGIN
 
 DECLARE nrRatos INT;
 
 SELECT numeroratosfinal into nrRatos
-FROM medicoespassagem
+FROM medicoessala
 WHERE idexperiencia = GetOngoingExpId() and sala = nrSala;
 
 return nrRatos;
